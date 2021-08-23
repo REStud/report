80ec90838fda5e76495a6024b17e2008a2c8afb1
Gergely Attila Kiss <corra971407@gmail.com>
2021-08-23 14:54:24 +0200

    finish up .py script, close #1

--- trello_json/trello_json_to_csv.py
+++ trello_json/trello_json_to_csv.py
@@ -4,7 +4,7 @@ import pandas as pd
 import json
 
 def read_json(file):
-    with open(jsonfile+'.json', 'r') as json_file:
+    with open(file+'.json', 'r') as json_file:
         json_data=json.load(json_file)
     return json_data
 
@@ -18,7 +18,7 @@ class Exporter(object):
         
 
     def cards_to_df(self):
-        columns = ['id', 'idList', 'closed', 'desc', 'name', 'labels']
+        columns = ['id', 'idList', 'closed', 'name', 'labels']
         self.cards = pd.DataFrame(self.json_data['cards']).loc[:, columns].set_index('id')
         cards_num = len(self.cards['labels'].values)     # get total number of cards to iterate
         # extract labels from dictionary
@@ -32,90 +32,85 @@ class Exporter(object):
         
 
     def merge_cards_to_lists(self):
-        self.cards = pd.merge(self.cards, self.lists, how='left', left_on='idList', right_on='id', 
+        self.cards = pd.merge(self.cards, self.lists_df, how='left', left_on='idList', right_on='id', 
                      suffixes=('_card', '_list'), right_index=True).drop(['idList'], axis=1)
 
-    def actions_to_df(self,actions_to_keep):
+    def actions_to_df(self,):
         columns = ['idMemberCreator','data','type','date']
         self.actions = pd.DataFrame(self.json_data['actions']).loc[:, columns]
         # filter DataFrame actions and sort in chronological order
-        self.actions = self.actions[self.actions['type'].isin(actions_to_keep)].reset_index(drop=True)
-        actions = actions.sort_values(by='date').reset_index(drop=True)
-        return actions
+        card_actions = ['createCard', 'updateCard'] # for some reason this did not work as a parameter passed when callingthe funciton.
+        self.actions = self.actions[self.actions['type'].isin(card_actions)].reset_index(drop=True)
+        self.actions = self.actions.sort_values(by='date').reset_index(drop=True)
 
-    def memebers_to_df(json_data):
-        members = pd.DataFrame(json_data['members']).loc[:, ['id', 'username']].set_index('id')
-        return members
-
-    def merge_actions_to_members(actions, members):
-        merged = pd.merge(actions, members, how='left',
-                       left_on='idMemberCreator', right_on='id',
-                       suffixes=('_actions', '_members')).drop(['idMemberCreator'], axis=1)
-        return merged
-
-    def fill_up_actions(actions):
+    def fill_up_actions(self):
         # prepare new columns
-        actions['card_id'] = ''
-        actions['changed'] = ''
-        actions['old'] = ''
-        actions['new'] = ''
+        self.actions['card_id'] = ''
+        self.actions['list_before'] = ''
+        self.actions['list_after'] = ''
 
         # add info to DataFrame
-        for row in range(len(actions)):
+        for row in range(len(self.actions)):
+            self.actions.loc[row, 'card_id'] = self.actions.loc[row, 'data']['card']['id']
             try:
-                actions.loc[row, 'card_id'] = actions.loc[row, 'data']['card']['id']
-                actions.loc[row, 'changed'] = list(actions.loc[row, 'data']['old'].keys())[0]
-                actions.loc[row, 'old'] = list(actions.loc[row, 'data']['old'].values())
-        # get value of what was changed to use it after
-                changed = list(actions.loc[row, 'data']['old'].keys())[0]
-                actions.loc[row, 'new'] = actions.loc[row, 'data']['card'][changed]
-            except:
-                pass
-        return actions
+                self.actions.loc[row, 'list_before'] = self.actions.loc[row, 'data']['listBefore']['name']
+                self.actions.loc[row, 'list_after'] = self.actions.loc[row, 'data']['listAfter']['name']
+            except KeyError:
+                self.actions.loc[row, 'list_before'] = ''
+                self.actions.loc[row, 'list_after'] = ''
 
-    def export_to_csv(cards, actions)
+    def export_to_csv(self):
         # create table for export
-        cards_list = []
-        for row in range(len(cards)):
+        self.cards_list = []
+        for row in range(len(self.cards)):
             # prepare empty dictionary
             card = {}
             
             # add name of card (task)
-            card['name'] = cards.iloc[row,2]
+            card['name'] = self.cards.iloc[row,1]
             
             # add status
             card['status'] = 'Open'
-            if cards.iloc[row, 0] == True:
+            if self.cards.iloc[row, 0] == True:
                 card['status'] = 'Closed'
             
             # add name of list (bucket)
-            card['list'] = cards.iloc[row, 4]
+            card['list'] = self.cards.iloc[row, 3]
             
             # add label
-            card['label'] = cards.iloc[row, 3]
+            card['label'] = self.cards.iloc[row, 2]
             
-            # add description
-            card['desc'] = cards.iloc[row, 1]
             
             # add actions to card
             # get card id
-            card_id = cards.index[row]
+            card_id = self.cards.index[row]
             
             # iterate over list of action to find action for this card
             counter = 0
-            for action_id in range(len(actions)):
-                if actions['card_id'][action_id] == card_id:
-                    card['activity' + str(counter)] = actions['type'][action_id]
-                    card['date' + str(counter)] = actions['date'][action_id][:16]
-                    card['user' + str(counter)] = actions['username'][action_id]
+            for action_id in range(len(self.actions)):
+                if self.actions['card_id'][action_id] == card_id:
+                    card['activity' + str(counter)] = self.actions['type'][action_id]
+                    card['date' + str(counter)] = self.actions['date'][action_id][:16]
+                    card['list_before' + str(counter)] = self.actions['list_before'][action_id]
+                    card['list_after' + str(counter)] = self.actions['list_after'][action_id]
                     counter += 1
             
             # add card dict to list of cards
-            cards_list.append(card)
+            self.cards_list.append(card)
             
         # prepare new DataFrame from list of dicts
-        df = pd.DataFrame(cards_list).set_index('name')
+        df = pd.DataFrame(self.cards_list).set_index('name')
         # export to CSV
         df.to_csv('table17aug21.csv')
 
 if __name__ == '__main__':
+
+    json_data = read_json('table17aug21')
+
+    exporter = Exporter(json_data)
+    exporter.lists_to_df()
+    exporter.cards_to_df()
+    exporter.merge_cards_to_lists()
+    exporter.actions_to_df()
+    exporter.fill_up_actions()
+    exporter.export_to_csv()
