ff42eb8c2eba23c7abdfd0a22061ea773f541067
Gergely Attila Kiss <corra971407@gmail.com>
2021-09-07 22:59:46 +0200

    finish up code and add zenodo_data, close #9

--- zenodo/zenodo_pull.py
+++ zenodo/zenodo_pull.py
@@ -1,15 +1,41 @@
 #need an empirical distribution of download and view numbers for zenodo packages
 import requests
 import json
-
+import csv
+import time
 class Zenodo(object):
     URL = f'https://zenodo.org/api/records/'
     def __init__(self, id):
         self.id = id
         self.meta = requests.get(self.__class__.URL + f'/{self.id}').json()
-        self.version = int(self.meta['metadata']['relations']['version'][0]['index']) + 1
-        self.num_versions = int(self.meta['metadata']['relations']['version'][0]['count'])
+
+
+def main():
+    with open('zenodoid.json', 'r') as json_file:
+            json_data = json.load(json_file)
+
+    zenodo_data = {'zenodoid': "", 'downloads': "", 'unique_downloads': "", 'views': "", 'unique_views': "", 'revisions':""}
+    zenodo = csv.DictWriter(open('zenodo_data.csv','w'), fieldnames=list(zenodo_data.keys()))
+    zenodo.writeheader()
+
+    for i in range(len(json_data)):
+        zenodoid = json_data[i]['zenodoid']
+        print(i)
+        zenodo_pack = Zenodo(zenodoid)
+        zenodo_data['zenodoid'] = zenodoid
+        zenodo_data['downloads'] = zenodo_pack.meta['stats']['downloads']
+        zenodo_data['unique_downloads'] = zenodo_pack.meta['stats']['unique_downloads']
+        zenodo_data['views'] = zenodo_pack.meta['stats']['views']
+        zenodo_data['unique_views'] = zenodo_pack.meta['stats']['unique_views']
+        zenodo_data['revisions'] = zenodo_pack.meta['revision']
+        zenodo.writerow(zenodo_data)
+
+        # The api calls are limited to 60/min therefore we have to wait before continue.
+        if (i+1)%60 == 0:
+            time.sleep(65)
+        else :
+            pass
+
 
 if __name__== '__main__':
-    zenodo = Zenodo(5338362)
-    print(zenodo.meta)
+    main()
